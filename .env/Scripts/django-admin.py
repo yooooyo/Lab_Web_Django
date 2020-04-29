a88dac9ccc5e49eb424e952ffac5be7e295268c9
tsyo <loipswqe@gmail.com>
2020-04-29 16:14:07 +0800

    -rrr

---
+++ .env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\ZapMaster PV\Documents\Work\HP_Project\Lab_Web_Django\.env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ .env/stubs.v4/bty7tx7J0YOedihYUTN1sWkiHVVrLrcSnO5mPWXtjTs=/_contextvars.pyi
@@ -0,0 +1,12 @@
+import builtins as _mod_builtins
+
+Context = _mod_builtins.Context
+ContextVar = _mod_builtins.ContextVar
+Token = _mod_builtins.Token
+__doc__ = 'Context Variables'
+__file__ = 'c:\\users\\zapmaster pv\\appdata\\local\\programs\\python\\python37\\dlls\\_contextvars.pyd'
+__name__ = '_contextvars'
+__package__ = ''
+def copy_context():
+    pass
+
